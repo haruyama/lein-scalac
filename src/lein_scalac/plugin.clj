0d295c2f71506a917c340aa4a940ce2277b061dc
Aaron Bernard <aaron.bernard@duramec.com>
2013-06-10 16:53:08 -0500

    bump lancet to 1.0.2

--- src/lein_scalac/plugin.clj
+++ src/lein_scalac/plugin.clj
@@ -3,7 +3,7 @@
 (defn middleware [project]
   (let [scala-version (:scala-version project)]
     (update-in project [:dependencies] concat
-               [['lancet "1.0.1"]
+               [['lancet "1.0.2"]
                 ['org.scala-lang/scala-compiler scala-version]
                 ['org.scala-lang/scala-library scala-version]])))
 
